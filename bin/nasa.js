7ef058652e49cf16e97541fb44580744d7299b82
Cam Pedersen <diffference@gmail.com>
2011-10-22 21:37:03 -0400

    better search

--- bin/nasa.js
+++ bin/nasa.js
@@ -16,10 +16,23 @@ program
 var methods = {
   search: function(){
     program.prompt('Search term: ', function(term){
-      console.log(':'+term);
       nasa.search({ search: term }, function(err, data){
-        console.log('worked');
-        console.log(err || data);
+        if (err) {
+          console.log(err);
+          process.exit();
+        } else if (data.posts) {
+          console.log(data.count + ' datasets were found matching "' + term + '"');
+          var titles = [];
+          for (var i in data.posts) {
+            titles.push(data.posts[i].title);
+          }
+          program.choose(titles, function(i){
+            console.log(data.posts[i]);
+          });
+        } else {
+          console.log('Sorry, there were no datasets found matching "' + term + '"');
+          methods.search();
+        }
       });
     });
   },
